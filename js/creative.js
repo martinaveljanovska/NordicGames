0f29e1bc875bb2d229e3c40fa9639a995de5159b
Stef Naskova <snaskova094@gmail.com>
2020-05-26 11:08:48 +0200

    edit:
    accordion code updated with the new one;
    everything is wrapped in a document ready function;
    redundant countdown code removed;

--- js/creative.js
+++ js/creative.js
@@ -1,39 +1,50 @@
-// accordion
-$(".toggle").click(function (e) {
-	e.preventDefault();
 
-	var toggle = $(this);
+$(function () {
 
-	if (toggle.next().hasClass("show")) {
-		toggle.next().removeClass("show");
-		toggle.next().slideUp(350);
-		toggle.removeClass("active");
-	} else {
-		toggle.parent().parent().find(".inner").removeClass("show");
-		// toggle.next().slideUp(350);
-		toggle.next().toggleClass("show");
-		toggle.next().slideToggle(350);
-		toggle.addClass("active");
-	}
-});
+	// ACCORDION 
+	$(".toggle").click(function (e) {
+		e.preventDefault();
+		let inner = $(this).next(".inner");
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(".page-scroll").bind("click", function (event) {
-	var $anchor = $(this);
-	$("html, body")
-		.stop()
-		.animate(
-			{
-				scrollTop: $($anchor.attr("href")).offset().top - 50,
-			},
-			250,
-			"easeOutSine"
-		);
-	event.preventDefault();
-});
+		if ($(this).hasClass("active")) {
+			inner.removeClass("show");
+			inner.slideUp(350);
+			$(this).removeClass("active");
+		} else {
+			$(this)
+				.closest(".accordion")
+				.find("a.active")
+				.removeClass("active");
+			$(this)
+				.closest(".accordion")
+				.find(".inner")
+				.not(inner)
+				.removeClass("show")
+				.slideUp(350);
+			inner.slideDown(350);
+			inner.addClass("show");
+			$(this).addClass("active");
+		}
+	});
 
-// back to top button
-$(document).ready(function () {
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(".page-scroll").bind("click", function (event) {
+		var $anchor = $(this);
+		$("html, body")
+			.stop()
+			.animate(
+				{
+					scrollTop: $($anchor.attr("href")).offset().top - 50,
+				},
+				250,
+				"easeOutSine"
+			);
+		event.preventDefault();
+	});
+
+
+	// back to top button
 	var offset = 550;
 	var duration = 300;
 
@@ -51,9 +62,8 @@ $(document).ready(function () {
 		$("html, body").animate({ scrollTop: 0 }, duration);
 		return false;
 	});
-});
 
-$(function () {
+
 	// MOBILE MENU
 	var toggle = $("#toggle");
 
@@ -73,36 +83,4 @@ $(function () {
 	});
 });
 
-// Countdown
-const second = 1000,
-	minute = second * 60,
-	hour = minute * 60,
-	day = hour * 24;
 
-let countDown = new Date("Sep 13, 2019 15:01:00").getTime(),
-	x = setInterval(function () {
-		let now = new Date().getTime(),
-			distance = countDown - now;
-
-		(document.getElementById("days").innerText = Math.floor(distance / day)),
-			(document.getElementById("hours").innerText = Math.floor(
-				(distance % day) / hour
-			)),
-			(document.getElementById("minutes").innerText = Math.floor(
-				(distance % hour) / minute
-			)),
-			(document.getElementById("seconds").innerText = Math.floor(
-				(distance % minute) / second
-			));
-
-		//do something later when date is reached
-		if (distance < 0) {
-			clearInterval(x);
-			(document.getElementById("days").innerText = 0),
-				(document.getElementById("hours").innerText = 0),
-				(document.getElementById("minutes").innerText = 0),
-				(document.getElementById("seconds").innerText = 0);
-		}
-	}, second);
-
-// countdown end
